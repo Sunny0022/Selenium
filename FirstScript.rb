120b5ab51d5b49166d9e7078bc75df4260ccbdb5
Sunny0022 <Sunny0022@users.noreply.github.com>
2020-05-19 18:02:54 +0530

    Update FirstScript.rb

--- FirstScript.rb
+++ FirstScript.rb
@@ -1,54 +1,64 @@
 require 'selenium-webdriver'
 require 'rubygems'
 
-  @driver = Selenium::WebDriver.for :firefox
-  @driver.get 'https://staging.dev.socialimprints.com'
-  sleep 35
-  @driver.manage.window.maximize
-  @driver.find_element(:xpath, '/html/body/div[9]/div[15]/aside/ul/li[7]/a').click
-  sleep 3
-  @driver.find_element(:xpath, '/html/body/div[9]/div[15]/div/article/aside/div/div[2]/div/button').click
-  @driver.find_element(:xpath, '/html/body/div[9]/div[15]/div/article/aside/div/div[2]/div/ul/li[1]/a').click
-  sleep 2
-  @driver.find_element(:css, 'div.form-control-group:nth-child(3) > input:nth-child(1)').send_keys('Test1234')
-  @driver.find_element(:xpath, '/html/body/div[9]/div[15]/div/div/div/div/form/article/aside/div[1]/div[4]/div[1]/div/div/span/span[1]/span/span[2]').click
-  @driver.find_element(:css, '.select2-search__field').send_keys('Sunny 2')
-  @driver.action.send_keys(:enter).perform
-  @driver.find_element(:id, 'accept_by_date').click
-  @driver.find_element(:id, 'accept_by_date').clear
-  @driver.find_element(:id, 'accept_by_date').send_keys('05/18/2020')
-  @driver.action.send_keys(:enter).perform
-  @driver.find_element(:id, 'accept_by_time').click
-  @driver.find_element(:xpath, '/html/body/div[9]/div[15]/div/div/div/div/form/article/aside/div[1]/div[5]/div[1]/div/div/div[2]/div/div/div[1]/div[2]/div[1]/div[12]').click
-  @driver.find_element(:xpath, '/html/body/div[9]/div[15]/div/div/div/div/form/article/aside/div[1]/div[5]/div[1]/div/div/div[2]/div/div/div[1]/div[2]/div[2]/div[1]').click
-  @driver.find_element(:xpath, '/html/body/div[9]/div[15]/div/div/div/div/form/article/aside/div[1]/div[5]/div[1]/div/div/div[2]/div/div/div[1]/div[2]/div[3]/div[2]').click
-  @driver.find_element(:xpath, '/html/body/div[9]/div[15]/div/div/div/div/form/article/aside/div[1]/div[5]/div[1]/div/div/div[2]/div/div/div[1]/div[2]/div[4]/div').click
-  @driver.find_element(:id, 'due_date').click
-  @driver.find_element(:id, 'due_date').clear
-  @driver.find_element(:id, 'due_date').send_keys('05/20/2020')
-  @driver.action.send_keys(:enter).perform
-  @driver.find_element(:xpath, '/html/body/div[9]/div[15]/div/div/div/div/form/article/aside/div[1]/div[5]/div[2]/div/div/div[2]/div/div/div[1]/div[2]/div[1]/div[1]').click
-  @driver.find_element(:xpath,'/html/body/div[9]/div[15]/div/div/div/div/form/article/aside/div[1]/div[5]/div[2]/div/div/div[2]/div/div/div[1]/div[2]/div[2]/div[2]').click
-  @driver.find_element(:xpath, '/html/body/div[9]/div[15]/div/div/div/div/form/article/aside/div[1]/div[5]/div[2]/div/div/div[2]/div/div/div[1]/div[2]/div[3]/div[1]').click
-  @driver.find_element(:xpath, '/html/body/div[9]/div[15]/div/div/div/div/form/article/aside/div[1]/div[5]/div[2]/div/div/div[2]/div/div/div[1]/div[2]/div[4]/div').click
-  @driver.find_element(:id, 'work_order_follower_autocomplete').send_keys('dheeraj+33@vinsol.com')
-  @driver.find_element(:id, 'estimated_work_time').send_keys('1:30')
-  @driver.find_element(:xpath, '/html/body/div[9]/div[15]/div/div/div/div/form/article/aside/div[1]/div[7]/div[1]/div/div/span/span[1]/span/span[2]').click
-  @driver.find_element(:css, '.select2-search__field').send_keys('Sunny account')
-  sleep 2
-  @driver.action.send_keys(:enter).perform
-  @driver.find_element(:xpath, '/html/body/div[9]/div[15]/div/div/div/div/form/article/aside/div[3]/div/div/div[2]/label[1]/span').click
-  @driver.find_element(:xpath, '/html/body/div[9]/div[15]/div/div/div/div/form/article/aside/div[4]/div[2]/div/div/div[1]/div/label[1]/span').click
-  @driver.find_element(:xpath, '/html/body/div[9]/div[15]/div/div/div/div/form/article/aside/div[4]/div[2]/div/div/div[1]/div/div[2]/div/label[1]/span').click
-  @driver.find_element(:id, 'form-id').click
-  sleep 10
-  @driver.find_element(:xpath, '/html/body/div[9]/div[15]/div/div/div[2]/form/article/aside/div[3]/div[2]/div[5]/div/div/div/button/span').click
-  sleep 2
-  @driver.find_element(:xpath, '/html/body/div[9]/div[15]/div/div/div[2]/form/article/aside/div[3]/div[2]/div[5]/div/div/div/ul/li[4]/a').click
-  sleep 2
-  @driver.find_element(:xpath, '/html/body/div[9]/div[15]/div/div/div[2]/form/article/aside/div[3]/div[2]/div[4]/div/article/div[1]/div/div[2]/div[1]/span[1]/span[1]/span/span[2]').click
-  @driver.find_element(:css, '.select2-search__field').send_keys('Blanket')
-  @driver.action.send_keys(:enter).perform
+@driver = Selenium::WebDriver.for :firefox
+@driver.get ("https://vinsol:v1ns0l@staging.dev.socialimprints.com")
+sleep 5
+@driver.find_element(:xpath, '/html/body/div[8]/section/div[2]/a').click
+sleep 4
+@driver.find_element(:css, '#identifierId').send_keys('aryasunny123@gmail.com')
+@driver.action.send_keys(:enter).perform
+sleep 2
+@driver.find_element(:css, '.I0VJ4d > div:nth-child(1) > input:nth-child(1)').send_keys("Rubyupon@123,./,./")
+@driver.action.send_keys(:enter).perform
+sleep 10
+@driver.manage.window.maximize
+@driver.find_element(:xpath, '/html/body/div[9]/div[15]/aside/ul/li[7]/a').click
+sleep 3
+@driver.find_element(:xpath, '/html/body/div[9]/div[15]/div/article/aside/div/div[2]/div/button').click
+@driver.find_element(:xpath, '/html/body/div[9]/div[15]/div/article/aside/div/div[2]/div/ul/li[1]/a').click
+sleep 2
+@driver.find_element(:css, 'div.form-control-group:nth-child(3) > input:nth-child(1)').send_keys('Blankets')
+@driver.find_element(:xpath, '/html/body/div[9]/div[15]/div/div/div/div/form/article/aside/div[1]/div[4]/div[1]/div/div/span/span[1]/span/span[2]').click
+@driver.find_element(:css, '.select2-search__field').send_keys('Sunny 2')
+@driver.action.send_keys(:enter).perform
+@driver.find_element(:id, 'work_order_follower_autocomplete').send_keys('dheeraj+33@vinsol.com')
+sleep 2
+@driver.find_element(:id, 'accept_by_date').click
+@driver.find_element(:id, 'accept_by_date').clear
+@driver.find_element(:id, 'accept_by_date').send_keys('05/19/2020')
+@driver.action.send_keys(:enter).perform
+@driver.find_element(:id, 'accept_by_time').click
+@driver.find_element(:xpath, '/html/body/div[9]/div[15]/div/div/div/div/form/article/aside/div[1]/div[5]/div[1]/div/div/div[2]/div/div/div[1]/div[2]/div[1]/div[12]').click
+@driver.find_element(:xpath, '/html/body/div[9]/div[15]/div/div/div/div/form/article/aside/div[1]/div[5]/div[1]/div/div/div[2]/div/div/div[1]/div[2]/div[2]/div[1]').click
+@driver.find_element(:xpath, '/html/body/div[9]/div[15]/div/div/div/div/form/article/aside/div[1]/div[5]/div[1]/div/div/div[2]/div/div/div[1]/div[2]/div[3]/div[2]').click
+@driver.find_element(:xpath, '/html/body/div[9]/div[15]/div/div/div/div/form/article/aside/div[1]/div[5]/div[1]/div/div/div[2]/div/div/div[1]/div[2]/div[4]/div').click
+
+@driver.find_element(:id, 'due_date').click
+@driver.find_element(:id, 'due_date').clear
+@driver.find_element(:id, 'due_date').send_keys('05/22/2020')
+@driver.action.send_keys(:enter).perform
+@driver.find_element(:xpath, '/html/body/div[9]/div[15]/div/div/div/div/form/article/aside/div[1]/div[5]/div[2]/div/div/div[2]/div/div/div[1]/div[2]/div[1]/div[1]').click
+@driver.find_element(:xpath,'/html/body/div[9]/div[15]/div/div/div/div/form/article/aside/div[1]/div[5]/div[2]/div/div/div[2]/div/div/div[1]/div[2]/div[2]/div[2]').click
+@driver.find_element(:xpath, '/html/body/div[9]/div[15]/div/div/div/div/form/article/aside/div[1]/div[5]/div[2]/div/div/div[2]/div/div/div[1]/div[2]/div[3]/div[1]').click
+@driver.find_element(:xpath, '/html/body/div[9]/div[15]/div/div/div/div/form/article/aside/div[1]/div[5]/div[2]/div/div/div[2]/div/div/div[1]/div[2]/div[4]/div').click
+@driver.find_element(:id, 'estimated_work_time').send_keys('1:30')
+@driver.find_element(:xpath, '/html/body/div[9]/div[15]/div/div/div/div/form/article/aside/div[1]/div[7]/div[1]/div/div/span/span[1]/span/span[2]').click
+@driver.find_element(:css, '.select2-search__field').send_keys('Sunny account')
+sleep 2
+@driver.action.send_keys(:enter).perform
+@driver.find_element(:xpath, '/html/body/div[9]/div[15]/div/div/div/div/form/article/aside/div[3]/div/div/div[2]/label[1]/span').click
+@driver.find_element(:xpath, '/html/body/div[9]/div[15]/div/div/div/div/form/article/aside/div[4]/div[2]/div/div/div[1]/div/label[1]/span').click
+@driver.find_element(:xpath, '/html/body/div[9]/div[15]/div/div/div/div/form/article/aside/div[4]/div[2]/div/div/div[1]/div/div[2]/div/label[1]/span').click
+@driver.find_element(:id, 'form-id').click
+sleep 10
+@driver.find_element(:xpath, '/html/body/div[9]/div[15]/div/div/div[2]/form/article/aside/div[3]/div[2]/div[5]/div/div/div/button/span').click
+sleep 2
+@driver.find_element(:xpath, '/html/body/div[9]/div[15]/div/div/div[2]/form/article/aside/div[3]/div[2]/div[5]/div/div/div/ul/li[4]/a').click
+sleep 2
+@driver.find_element(:xpath, '/html/body/div[9]/div[15]/div/div/div[2]/form/article/aside/div[3]/div[2]/div[4]/div/article/div[1]/div/div[2]/div[1]/span[1]/span[1]/span/span[2]').click
+@driver.find_element(:css, '.select2-search__field').send_keys('Blanket')
+@driver.action.send_keys(:enter).perform
   sleep 2
   @driver.find_element(:id, 'embroidery_name_0').send_keys('Home made Blanket')
   @driver.find_element(:id, 'embroidery_color_0').send_keys('Blue, Black, Brown')
@@ -62,6 +72,9 @@ require 'rubygems'
   sleep 2
   @driver.find_element(:id, 'color_name100').send_keys('Black')
   @driver.find_element(:id, 'form-id').click
-  sleep 15
-  @driver.quit
+  sleep 5
+@driver.find_element(:xpath, '/html/body/div[9]/header/article/div/div/aside[2]/dd/dl[3]/div/a/span').click
+@driver.find_element(:xpath, '/html/body/div[9]/header/article/div/div/aside[2]/dd/dl[3]/div/ul/li[4]/form').click
+sleep 5
+@driver.quit
   puts 'Work order has been successfully created!!!'
